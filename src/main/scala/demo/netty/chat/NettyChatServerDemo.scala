df74657f421148c90f849b53417cae3c6bbe58dc
ning_guanyi <ningguanyi@haima.me>
2021-05-13 14:34:47 +0800

    Netty心跳检测

--- src/main/scala/demo/netty/chat/NettyChatServerDemo.scala
+++ src/main/scala/demo/netty/chat/NettyChatServerDemo.scala
@@ -6,6 +6,9 @@ import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioServerSocketChannel
 import io.netty.handler.codec.string.{StringDecoder, StringEncoder}
+import io.netty.handler.timeout.IdleStateHandler
+
+import java.util.concurrent.TimeUnit
 
 object NettyChatServerDemo {
   val port = 6678
@@ -21,8 +24,24 @@ object NettyChatServerDemo {
         .childHandler(new ChannelInitializer[SocketChannel] {
           override def initChannel(ch: SocketChannel): Unit = {
             val pipeline = ch.pipeline()
-            pipeline.addLast("decoder", new StringDecoder());
-            pipeline.addLast("encoder", new StringEncoder());
+            //加入一个netty 提供 IdleStateHandler
+            /*
+            说明
+            1. IdleStateHandler 是netty 提供的处理空闲状态的处理器
+            2. long readerIdleTime : 表示多长时间没有读, 就会发送一个心跳检测包检测是否连接。0不检测
+            3. long writerIdleTime : 表示多长时间没有写, 就会发送一个心跳检测包检测是否连接。0不检测
+            4. long allIdleTime : 表示多长时间没有读写, 就会发送一个心跳检测包检测是否连接。0不检测
+
+            5. 文档说明
+            triggers an {@link IdleStateEvent} when a {@link Channel} has not performed
+            read, write, or both operation for a while.
+            6. 当 IdleStateEvent 触发后 , 就会传递给管道 的下一个handler去处理，通过调用(触发)
+            下一个handler 的 userEventTiggered , 在该方法中去处理 IdleStateEvent(读空闲，写空闲，读写空闲)
+            7.handlerRemoved有时候是无法感知连接断掉，所以还是需要心跳包的检测来判断连接是否还有效
+             */
+            pipeline.addLast(new IdleStateHandler(3, 5, 7, TimeUnit.SECONDS))
+            pipeline.addLast("decoder", new StringDecoder())
+            pipeline.addLast("encoder", new StringEncoder())
             pipeline.addLast(new NettyChatServerHandler())
           }
         })
--- src/main/scala/demo/netty/chat/NettyChatServerHandler.scala
+++ src/main/scala/demo/netty/chat/NettyChatServerHandler.scala
@@ -4,6 +4,7 @@ package demo.netty.chat
 import demo.netty.chat.NettyChatServerHandler.{channels, uid}
 import io.netty.channel.group.DefaultChannelGroup
 import io.netty.channel.{Channel, ChannelHandlerContext, ChannelId, SimpleChannelInboundHandler}
+import io.netty.handler.timeout.{IdleState, IdleStateEvent}
 import io.netty.util.concurrent.GlobalEventExecutor
 
 import java.text.SimpleDateFormat
@@ -42,9 +43,9 @@ class NettyChatServerHandler extends SimpleChannelInboundHandler[String] {
    * 断开连接, 将xx客户离开信息推送给当前在线的客户
    */
   override def handlerRemoved(ctx: ChannelHandlerContext): Unit = {
-    val channel = ctx.channel();
-    channelGroup.writeAndFlush("[客户端]" + channel.remoteAddress() + " 离开了\n");
-    println("channelGroup size" + channelGroup.size());
+    val channel = ctx.channel()
+    channelGroup.writeAndFlush("[客户端]" + channel.remoteAddress() + " 离开了\n")
+    println("channelGroup size" + channelGroup.size())
     val item = channels.find(entry => entry._2 == channel)
     if (item.nonEmpty) {
       channels -= item.get._1
@@ -63,7 +64,7 @@ class NettyChatServerHandler extends SimpleChannelInboundHandler[String] {
    * 表示channel 处于不活动状态, 提示 xx 离线了
    */
   override def channelInactive(ctx: ChannelHandlerContext): Unit = {
-    println(ctx.channel().remoteAddress() + "离线了~");
+    println(ctx.channel().remoteAddress() + "离线了~")
   }
 
   /**
@@ -71,20 +72,37 @@ class NettyChatServerHandler extends SimpleChannelInboundHandler[String] {
    */
   override def channelRead0(ctx: ChannelHandlerContext, msg: String): Unit = {
     //获取到当前channel
-    val channel = ctx.channel();
+    val channel = ctx.channel()
     //这时我们遍历channelGroup, 根据不同的情况，回送不同的消息
     val iter = channelGroup.iterator()
     while (iter.hasNext) {
       val ch = iter.next()
       if(channel != ch) { //不是当前的channel,转发消息
-        ch.writeAndFlush("[客户]" + channel.remoteAddress() + " 发送了消息" + msg + "\n");
+        ch.writeAndFlush("[客户]" + channel.remoteAddress() + " 发送了消息" + msg + "\n")
       }else {//回显自己发送的消息给自己
-        ch.writeAndFlush("[自己]发送了消息" + msg + "\n");
+        ch.writeAndFlush("[自己]发送了消息" + msg + "\n")
       }
     }
   }
-
+  @SuppressWarnings(Array("deprecation"))
   override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = {
     ctx.close()
+  }
+  override def userEventTriggered(ctx: ChannelHandlerContext, evt: Any): Unit = {
+    evt match {
+      case event: IdleStateEvent =>
+        val eventType = event.state() match {
+          case IdleState.READER_IDLE =>
+            "读空闲"
+          case IdleState.WRITER_IDLE =>
+            "写空闲"
+          case IdleState.ALL_IDLE =>
+            "读写空闲"
+        }
+        println(ctx.channel().remoteAddress() + "--超时时间--" + eventType)
+        println("服务器做相应处理..")
+    }
+    //如果发生空闲，我们关闭通道
+    // ctx.channel().close();
   }
 }
